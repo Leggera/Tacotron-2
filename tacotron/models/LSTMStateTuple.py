34ee96012a90959f4dd9cdc1870d3e7516e02ee4
Leggera <lana.krlv1995@gmail.com>
2019-06-11 22:17:50 +0300

    Update LSTMStateTuple.py

--- tacotron/models/LSTMStateTuple.py
+++ tacotron/models/LSTMStateTuple.py
@@ -1,4 +1,7 @@
 from tensorflow.python.util.tf_export import tf_export
+import collections
+
+_LSTMStateTuple = collections.namedtuple("LSTMStateTuple", ("c", "h"))
 
 @tf_export("nn.rnn_cell.LSTMStateTuple")
 class LSTMStateTuple(_LSTMStateTuple):
