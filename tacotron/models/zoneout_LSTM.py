98effbd81a5391dbaded3611faff81f47cba0f84
Leggera <lana.krlv1995@gmail.com>
2019-06-11 22:16:48 +0300

    Update zoneout_LSTM.py

--- tacotron/models/zoneout_LSTM.py
+++ tacotron/models/zoneout_LSTM.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 #import tensorflow_addons as tfa
 #from tensorflow.python.ops.rnn_cell import RNNCell
-from LSTMStateTuple import LSTMStateTuple
+from tacotron.models.LSTMStateTuple import LSTMStateTuple
 from tensorflow_addons.rnn import cell as rnn_cell
 from tensorflow.keras.layers import AbstractRNNCell as RNNCell
 
